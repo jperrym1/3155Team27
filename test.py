0b780dff8df03aa6b46a289e90ba04629a0f7cc5
Howstheaqua <hughes.e.47201@gmail.com>
2022-11-17 13:49:25 -0500

    testing

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ class ProjectTest:
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>This site is used to manage and organize Tasks and Projects</h2>' in response.text, True)
 
     def test_delete_method(self):
-        pass
+        self.assertEqual(len(ProjectV1.projects), 3)
+        ProjectV1.delete_project(1)
