93e71f6c6a7299a308d774b1dbc040ffc03140e9
Howstheaqua <hughes.e.47201@gmail.com>
2022-11-17 13:58:30 -0500

    name changes

--- ProjectV1.py
+++ ProjectV1.py
@@ -12,6 +12,7 @@ projects = {1: {'name': 'project1', 'description': 'first mock project on the li
             2: {'name': 'project2', 'description': 'second mock project', 'members': 'Sam, Gina, Tom'},
             3: {'name': 'project3', 'description': 'third mock project', 'members': 'Tina, Dana, Fred'} 
 }
+tempUser = {'name': 'admin', 'email': 'admin@3cubed.com'}
 #main menu
 @app.route('/')
 @app.route('/index')
@@ -24,13 +25,13 @@ def index():
 @app.route('/projects')
 def get_projects():
 
-    return render_template('projects.html', projects=projects)
+    return render_template('projects.html', projects=projects, user=tempUser)
 
 #view particular project
 @app.route('/projects/<project_id>')
-def get_project():
+def get_project(project_id):
 
-    return render_template('project.html')
+    return render_template('project.html', project=projects[int(project_id)],user=tempUser)
 
 #create project
 @app.route('/projects/new')
